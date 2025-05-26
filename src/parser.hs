8c3ce6deb5a10d837c22686de1bedcaa6d9734ed
jarinios <72438550+jarinios@users.noreply.github.com>
2025-05-26 21:15:23 +0200

    Update parser.hs
    New parser to:
    - PauliX
    - PauliY
    - PauliZ
    - CNOT

--- src/parser.hs
+++ src/parser.hs
@@ -28,6 +28,10 @@ data Program = Program [Statement]
 data Statement
   = InitQubit Int
   | Hadamard Int
+  | PauliX Int
+  | PauliY Int
+  | PauliZ Int
+  | CNOT Int Int
   deriving (Show, Eq)
 
 programParser :: Parser Program
@@ -37,14 +41,74 @@ programParser = do
   eof
   return $ Program stmts
 
-
 statementParser :: Parser Statement
 statementParser = do
   skipMany space
-  stmt <- try initParser <|> try hadamardParser
+  stmt <- try initParser <|> try hadamardParser <|> try pauliXParser <|> try pauliYParser <|> try pauliZParser <|> try cnotParser
   skipMany (oneOf " \t")
   optional newline
   return stmt
+
+cnotParser :: Parser Statement
+cnotParser = do
+  skipMany space
+  _ <- string "CNOT"
+  skipMany1 space
+  n <- intParser
+  skipMany space
+  m <- intParser
+  skipMany space
+  optional (try (string "//" >> many (noneOf "\n")))
+  skipMany (oneOf " \t")
+  optional newline
+  return $ CNOT n m
+
+pauliZParser :: Parser Statement
+pauliZParser = do
+  skipMany space
+  _ <- string "PauliZ"
+  skipMany1 space
+  n <- intParser
+  skipMany space
+  optional (try (string "//" >> many (noneOf "\n")))
+  skipMany (oneOf " \t")
+  optional newline
+  return $ PauliZ n
+
+pauliYParser :: Parser Statement
+pauliYParser = do
+  skipMany space
+  _ <- string "PauliY"
+  skipMany1 space
+  n <- intParser
+  skipMany space
+  optional (try (string "//" >> many (noneOf "\n")))
+  skipMany (oneOf " \t")
+  optional newline
+  return $ PauliY n
+
+pauliXParser :: Parser Statement
+pauliXParser = do
+  skipMany space
+  _ <- string "PauliX"
+  skipMany1 space
+  n <- intParser
+  skipMany space
+  optional (try (string "//" >> many (noneOf "\n")))
+  skipMany (oneOf " \t")
+  optional newline
+  return $ PauliX n
+
+hadamardParser :: Parser Statement
+hadamardParser = do
+  skipMany space
+  _ <- string "HADAMARD"
+  skipMany1 space
+  n <- intParser
+  optional (try (string "//" >> many (noneOf "\n")))
+  skipMany (oneOf " \t")
+  optional newline
+  return $ Hadamard n
 
 initParser :: Parser Statement
 initParser = do
@@ -58,26 +122,21 @@ initParser = do
   optional newline
   return $ InitQubit n
 
-hadamardParser :: Parser Statement
-hadamardParser = do
-  skipMany space
-  _ <- string "HADAMARD"
-  skipMany1 space
-  n <- intParser
-  optional (try (string "//" >> many (noneOf "\n")))
-  skipMany (oneOf " \t")
-  optional newline
-  return $ Hadamard n
 
 intParser :: Parser Int
 intParser = read <$> many1 digit
 
 exampleCode :: String
 exampleCode = unlines
-  [ "INIT 1", "INIT 3", "HADAMARD 2"]
+  [ "INIT 1", "INIT 3 // let the force be with you, but it will be skipped", "HADAMARD 2", "PauliX 4", "PauliY 5 ", "PauliZ 6", "CNOT 3 4"]
 
 main :: IO ()
 main = do
   case parse programParser "" exampleCode of
     Left err -> putStrLn $ "Parse error:\n" ++ show err
     Right ast -> print ast
+
+    -- these lines below are applied to parsers functions and skips comments
+  --optional (try (string "//" >> many (noneOf "\n")))
+  --skipMany (oneOf " \t")
+  --optional newline
