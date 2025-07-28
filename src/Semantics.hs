14c22d0b56bb3154d3ee0c170a65e7ddf215eccc
Jarugam <jarugam@gamil.com>
2025-07-28 22:13:53 +0200

    Add Cnot interpreter for unentangled qubits

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -32,7 +32,8 @@ instance Show Value where
 
 data Environment = Env
                     { vars :: Map String Value
-                    , qubits :: [[Complex Double]] }
+                    , qubits :: [[Complex Double]]
+                    , entangledId :: [[Int]]}
 
 type EvalM a = StateT Environment IO a
 
@@ -71,6 +72,44 @@ execStmt stmt = do
                         ]
             applySingleGate "Pauli Y" pauliY qubitId
 
+        PauliZ qubitId -> do
+            let pauliZ = fromLists
+                        [ [1 :+ 0, 0 :+ 0]
+                        , [0 :+ 0, (-1) :+ 0]
+                        ]
+            applySingleGate "Pauli Z" pauliZ qubitId
+
+        CNOT controlQubitId targetQubitId -> do
+            env <- get
+            if length (qubits env) <= controlQubitId || controlQubitId < 0 ||
+                length (qubits env) <= targetQubitId || targetQubitId < 0
+                then liftIO $ putStrLn $ "No valid qubit with ID = " ++ show controlQubitId ++ " , or ID = " ++ show targetQubitId
+            else do
+                let cnot = fromLists
+                            [ [1 :+ 0, 0 :+ 0, 0 :+ 0, 0 :+ 0]
+                            , [0 :+ 0, 1 :+ 0, 0 :+ 0, 0 :+ 0]
+                            , [0 :+ 0, 0 :+ 0, 0 :+ 0, 1 :+ 0]
+                            , [0 :+ 0, 0 :+ 0, 1 :+ 0, 0 :+ 0]
+                            ]
+                    qubitList = qubits env
+                    controlQubit = qubitList !! controlQubitId
+                    targetQubit = qubitList !! targetQubitId
+                if isBasisState controlQubit && isBasisState targetQubit then do
+                    case magnitude $ last controlQubit of
+                        1 -> do
+                            let xGate = fromLists [ [0 :+ 0, 1 :+ 0], [1 :+ 0, 0 :+ 0]]
+                                newTarget = applyGate xGate targetQubit
+                                newQubits = replace targetQubitId newTarget qubitList
+                            put env {qubits = newQubits}
+                            liftIO $ putStrLn $ "Cnot gate applied with cotrol = " ++ show controlQubitId ++ " and target = " ++ show targetQubitId
+                        _ -> do
+                            liftIO $ putStrLn "Qubits left unchanged (ket 0 as control case)"
+                else do
+                    liftIO $ putStrLn "Cnot gate for entangled qubits remains to be implemented"
+
+
+
+
     currentState <- get
     liftIO $ putStrLn $ "Current state vector: " ++ show (qubits currentState) ++ "\n--------------------------------------------"
 
@@ -87,7 +126,7 @@ execStmt stmt = do
         applyGate :: Matrix (Complex Double) -> [Complex Double] -> [Complex Double]
         applyGate gateMatrix qubitVector = toList $ multStd gateMatrix (colVector (V.fromList qubitVector))
 
-        applySingleGate :: String -> Matrix (Complex Double) -> Int -> EvalM () 
+        applySingleGate :: String -> Matrix (Complex Double) -> Int -> EvalM ()
         applySingleGate gateName gateMatrix qubitId = do
             env <- get
             if length (qubits env) <= qubitId || qubitId < 0 then liftIO $ putStrLn $ "No valid qubit with ID = " ++ show qubitId
@@ -96,6 +135,16 @@ execStmt stmt = do
                     transformedQubit = applyGate gateMatrix qubit
                 put env {qubits = replace qubitId transformedQubit (qubits env)}
                 liftIO $ putStrLn $ gateName ++ " gate used on " ++ show qubitId ++ "-th qubit"
+
+        isBasisState :: [Complex Double] -> Bool
+        isBasisState [a, b] = (magnitude a == 1 && magnitude b == 0) || (magnitude a == 0 && magnitude b == 1)
+        isBasisState _ = False
+
+        isEntangled :: Int -> EvalM Bool
+        isEntangled qubitId' = do
+            env <- get
+            let entagledList = entangledId env
+            return $ any (qubitId' `elem`) entagledList
 
 
 execProgram :: [Statement] -> EvalM ()
@@ -106,7 +155,7 @@ runProgram list = do
     putStrLn "Running the program...\n--------------------------------------------"
     runStateT (execProgram list) initialEnv
     where
-        initialEnv = Env {vars = Map.empty ,qubits = []}
+        initialEnv = Env {vars = Map.empty ,qubits = [], entangledId = []}
 
 example1 :: [Statement]
-example1 = [InitQubit 2, Hadamard 1, PauliY 0]
+example1 = [InitQubit 2, PauliX 1, CNOT 0 1]
